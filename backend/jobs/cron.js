f302b4542fcc797eb4f19653c068c8c1ab40770b
Nguyen Ho Tien Dat <130582440+nhtdat07@users.noreply.github.com>
2025-05-06 11:49:48 +0700

    Config GMT time for cron job (#60)

--- backend/jobs/cron.js
+++ backend/jobs/cron.js
@@ -3,7 +3,7 @@ import { pool } from '../db.js';
 import { checkInventory } from './check_inventory.js';
 
 // Checking inventory for all user agencies at 02:00 GMT+7 every day
-cron.schedule('40 4 * * *', async () => {
+cron.schedule('0 19 * * *', async () => {
     console.log('[CRON] Running inventory check...');
     await checkInventory(pool);
     console.log('[CRON] Finish inventory check');
