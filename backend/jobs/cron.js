ce4e7bb92fe8a97118d559a0122f23dd30a23ca2
Nguyen Ho Tien Dat <130582440+nhtdat07@users.noreply.github.com>
2025-05-06 11:23:14 +0700

    Test cron job (#59)

--- backend/jobs/cron.js
+++ backend/jobs/cron.js
@@ -2,8 +2,8 @@ import cron from 'node-cron';
 import { pool } from '../db.js';
 import { checkInventory } from './check_inventory.js';
 
-// Checking inventory for all user agencies at 02:00 every day
-cron.schedule('50 20 * * *', async () => {
+// Checking inventory for all user agencies at 02:00 GMT+7 every day
+cron.schedule('40 4 * * *', async () => {
     console.log('[CRON] Running inventory check...');
     await checkInventory(pool);
     console.log('[CRON] Finish inventory check');
