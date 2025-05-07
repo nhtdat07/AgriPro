5a7276084f6ed4b6ae587243559bcbf71a6ad57a
quocthanh03 <quocthanh.280603@gmail.com>
2025-05-07 22:31:17 +0700

    Update logout

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -7,10 +7,11 @@ import { useNavigate } from "react-router-dom";
 function Sidebar({ ...props }) {
   const navigate = useNavigate();
   const [menus] = useState(props.menu);
+  const [showLogoutModal, setShowLogoutModal] = useState(false);
 
-  const logout = () => {
-    localStorage.removeItem("token"); // Xóa token
-    navigate("/"); // Điều hướng về trang login
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/");
   };
 
   return (
@@ -27,7 +28,7 @@ function Sidebar({ ...props }) {
             <div className="py-2 px-2">
               <button
                 className="py-2 px-2 w-full text-[#c91e1e] justify-end font-medium"
-                onClick={logout}
+                onClick={() => setShowLogoutModal(true)}
               >
                 ĐĂNG XUẤT
               </button>
@@ -41,8 +42,31 @@ function Sidebar({ ...props }) {
           id="overlaySidebar"
           onClick={props.toggle}
           className="hidden absolute w-full h-screen bg-black z-10 inset-0 opacity-60"
-        >
-          <div></div>
+        />
+      )}
+
+      {showLogoutModal && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white rounded-lg shadow-md w-[500px] p-6">
+            <h3 className="text-2xl font-semibold text-center">Xác nhận</h3>
+            <p className="my-4 text-gray-700 text-lg leading-relaxed text-center">
+              Bạn có chắc chắn muốn đăng xuất?
+            </p>
+            <div className="flex justify-center gap-4 p-4">
+              <button
+                className="bg-[#2c9e4b] hover:bg-[#0c5c30] text-white px-6 py-2 rounded-lg"
+                onClick={handleLogout}
+              >
+                XÁC NHẬN
+              </button>
+              <button
+                className="bg-[#2c9e4b] hover:bg-[#0c5c30] text-white px-6 py-2 rounded-lg"
+                onClick={() => setShowLogoutModal(false)}
+              >
+                TRỞ LẠI
+              </button>
+            </div>
+          </div>
         </div>
       )}
     </>
